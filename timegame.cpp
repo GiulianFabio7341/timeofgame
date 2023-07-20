03689d3a8a4f7d88e0ebc79abdd535166c2d62fb
Giulian Fabio <102571267+giulianfabio@users.noreply.github.com>
2023-07-20 11:49:37 -0300

    Update timegame.cpp

--- timegame.cpp
+++ timegame.cpp
@@ -2,17 +2,45 @@
 using namespace std;
 
 int main() {
-	int hi,hf,horas;
-	cin>>hi>>hf;
-	if(hi<hf){
+	int hi,mi,hf,mf,horas,minutos;
+	cin>>hi>>mi>>hf>>mf;
+
+	if(hi<hf&&mi<mf){
 	horas=hf-hi;
-	}
-	if(hi>hf){
-	horas=(24-hi)+hf;
-	}
-	if(hi==hf){
+	minutos=mf-mi;}
+	
+    if(hi<hf&&mi>mf){
+	horas=(hf-hi)-1;
+	minutos=(60-mi)+mf;}
+		
+    if(hi<hf&&mf==mi){
+	horas=hf-hi;
+	minutos=0;}
+
+	if(hi==hf&&mi==mf){
 	horas=24;
-	}
-	cout<<"O JOGO DUROU "<<horas<< " HORA(S)"<<endl;
+	minutos=0;}
+
+    if(hi==hf&&mf>mi){
+	horas=0;
+	minutos=mf-mi;}
+	
+	if(hi==hf&&mf<mi){
+	horas=23;
+	minutos=(60-mi)+mf;}
+	
+	if(hi>hf&&mf==mi){
+	horas=(24-hi)+hf;
+	minutos=0;}
+		
+	if(hi>hf&&mf>mi){
+   	horas=(24-hi)+hf;
+    minutos=mf-mi;}
+	    
+	if(hi>hf&&mf<mi){
+	 horas=(24-hi)+hf-1;
+	 minutos=(60-mi)+mf;}
+
+	cout<< "O JOGO DUROU "<<horas<<" HORA(S)"<<" E "<<minutos<< " MINUTO(S)"<<endl;
 	return 0;
 }
